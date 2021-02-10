2026a4355c866ffe402e48f2bb42f57f6bae017e
WataruOjima <wataru.ojima20@gmail.com>
2021-02-10 23:50:19 +0900

    同じコードをメソッドにまとめ

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:show, :edit]
   def index 
     @tweets = Tweet.all
   end
@@ -17,7 +18,6 @@ class TweetsController < ApplicationController
   end 
 
   def edit
-    @tweet = Tweet.find(params[:id])
   end 
 
   def update
@@ -26,13 +26,16 @@ class TweetsController < ApplicationController
   end 
 
   def show
-    @tweet = Tweet.find(params[:id])
   end 
 
   private 
 
   def tweet_params
     params.require(:tweet).permit(:name, :text, :image)
+  end 
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end 
 
 end
